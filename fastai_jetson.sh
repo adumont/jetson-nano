2ea3d83c5e14479662f6115924b41689cf0e2b83
Alexandre Dumont <adumont@gmail.com>
2019-07-08 23:53:38 +0200

    updated PyTorch, torchvision

--- fastai_jetson.sh
+++ fastai_jetson.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install build-essential python-dev git
 sudo apt-get -y install python-requests python-typing
 sudo apt-get -y install g++ cmake python-dev
 sudo apt-get -y install nodejs npm freetype2-demos
-sudo apt-get -y install zlib1g-dev zip libjpeg8-dev libhdf5-dev libpng-devel libfreetype-devel
+sudo apt-get -y install zlib1g-dev zip libjpeg8-dev libhdf5-dev libpng-dev libfreetype6-dev
 sudo apt-get -y install libhdf5-serial-dev hdf5-tools
 sudo apt-get -y install cython python-cuda
 sudo apt-get -y install pkg-config libfreetype6-dev libpng12-dev
@@ -18,17 +18,31 @@ pip3 install freetype-py
 pip3 install pypng 
 pip3 install dataclasses bottleneck
 pip3 install pynvx
-pip3 install pandas
+#pip3 install pandas
 pip3 install fire
 pip3 install jupyter jupyterlab
 pip3 install fastprogress
 
+# TensorFlow
 #pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu
 #pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==$TF_VERSION+nv$NV_VERSION
 
-wget https://nvidia.box.com/shared/static/veo87trfaawj5pfwuqvhl6mzc5b55fbj.whl -O torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-pip3 install torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
-pip3 install torchvision --no-deps
+# Updated PyTorch, see https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/
+
+wget https://nvidia.box.com/shared/static/j2dn48btaxosqp0zremqqm8pjelriyvs.whl -O torch-1.1.0-cp36-cp36m-linux_aarch64.whl
+pip3 install numpy torch-1.1.0-cp36-cp36m-linux_aarch64.whl
+
+#pip3 install torchvision --no-deps
+
+# Build & Install Torchvision
+# see https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/
+sudo apt-get install libjpeg-dev zlib1g-dev
+git clone -b v0.3.0 https://github.com/pytorch/vision torchvision
+cd torchvision
+sudo python3 setup.py install
+cd ..
+rm -rf torchvision
+
 pip3 install spacy==2.0.18
 
 pip3 install fastai --no-deps
