29b86230367479176e94794c7d940701198c855f
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2019-08-02 00:34:29 +0200

    Update setup_fastai.sh

--- setup_fastai.sh
+++ setup_fastai.sh
@@ -12,14 +12,18 @@ sudo apt-get -y install zlib1g-dev zip libjpeg8-dev libhdf5-dev libpng-dev libfr
 sudo apt-get -y install libhdf5-serial-dev hdf5-tools
 sudo apt-get -y install cython python-cuda
 sudo apt-get -y install pkg-config libfreetype6-dev #libpng12-dev
+sudo apt-get install -yy libjpeg-dev zlib1g-dev
 #sudo apt-get -y install htop
 
 pip3 install freetype-py
-pip3 install pypng 
-pip3 install dataclasses bottleneck
-pip3 install pynvx
-#pip3 install pandas
-pip3 install fire
+pip3 install pypng
+
+pip3 install dist/{numpy,Bottleneck,dataclasses}*
+pip3 install dataclasses
+
+pip3 install dist/{pynvx,fire,termcolor,tornado,prometheus-client}*
+pip3 install dist/{pyzmq,pandocfilters,backcall,MarkupSafe,pyrsistent}*
+
 pip3 install jupyter jupyterlab
 pip3 install fastprogress
 
@@ -30,24 +34,25 @@ pip3 install fastprogress
 # Updated PyTorch, see https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/
 
 wget https://nvidia.box.com/shared/static/j2dn48btaxosqp0zremqqm8pjelriyvs.whl -O /tmp/torch-1.1.0-cp36-cp36m-linux_aarch64.whl
-pip3 install numpy /tmp/torch-1.1.0-cp36-cp36m-linux_aarch64.whl && rm /tmp/torch-1.1.0-cp36-cp36m-linux_aarch64.whl
+pip3 install /tmp/torch-1.1.0-cp36-cp36m-linux_aarch64.whl && rm /tmp/torch-1.1.0-cp36-cp36m-linux_aarch64.whl
 
 # pip3 install torchvision --no-deps
 
 # Build & Install Torchvision
 # see https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/
-sudo apt-get install libjpeg-dev zlib1g-dev
 
 #git clone -b v0.3.0 https://github.com/pytorch/vision torchvision
 #cd torchvision
 #sudo python3 setup.py install
 #cd ..
 #rm -rf torchvision
-pip3 install dist/torchvision-0.3.0-cp36-cp36m-linux_aarch64.whl
 
-pip3 install spacy==2.0.18
+pip3 install dist/{six,Pillow}*
+pip3 install dist/torchvision-0.3.0-cp36-cp36m-linux_aarch64.whl --no-deps
 
-pip3 install nvidia-ml-py3
+pip3 install dist/{murmurhash,cymem,preshed,msgpack,toolz,cytoolz}*
+pip3 install dist/{wrapt,dill,thinc,ujson,regex,spacy,nvidia_ml_py3}*
+
 pip3 install packaging
 pip3 install typing
 
