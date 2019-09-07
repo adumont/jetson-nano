3aecb52705d732936a3384d287c20809f749564a
Alexandre Dumont <adumont@gmail.com>
2019-09-07 23:28:36 +0200

    added cv2 to version check

--- check_versions.sh
+++ check_versions.sh
@@ -1,4 +1,4 @@
-for i in numpy pandas jupyter fastai torch torchvision tensorflow keras
+for i in cv2 numpy pandas jupyter fastai torch torchvision tensorflow keras
 do
    python3 -c "import $i; print('$i: ' + $i.__version__)"
 done
