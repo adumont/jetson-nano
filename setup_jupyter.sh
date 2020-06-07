2a35073237ba7a5859979da829da06633c514d3f
Alexandre Dumont <adumont@gmail.com>
2020-06-08 01:32:09 +0200

    modified

--- setup_jupyter.sh
+++ setup_jupyter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install @jupyterlab/statusbar
+jupyter serverextension enable --py nbresuse
+#jupyter labextension install @jupyterlab/statusbar
 jupyter lab --generate-config
-cp jupyter_notebook_config.py .jupyter/jupyter_notebook_config.py
-jupyter notebook password
+cp jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+#jupyter notebook password
